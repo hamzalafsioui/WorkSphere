ca9cbc188b5ad77359c8ac5d5af990d879fd3866
hamzalafsioui <hamzalafsioui@gmail.com>
2025-11-17 17:06:47 +0100

    store employee to local storage

--- scripts/script.js
+++ scripts/script.js
@@ -4,29 +4,65 @@ let zones = document.querySelectorAll(".zone");
 console.log(zones);
 // ============== Local Storage ================
 
-
 // ================= Modals ===================
-const workerModal = document.getElementById("add-worker-modal");
-
+const workerModal = document.getElementById("modal-add-worker");
 
 // ================= Buttons ===================
-const btnAddWorker = document.getElementById("open-modal-btn");
+const btnAddWorker = document.getElementById("btn-open-modal");
 const btnCloseModal = document.getElementById("btn-close-modal");
-
+const btnSaveEmployee = document.getElementById("btn-save-employee");
 // ================= Input =====================
-
+const nameInput = document.getElementById("name-input");
+const phoneInput = document.getElementById("phone-input");
+const emailInput = document.getElementById("email-input");
+const photoInput = document.getElementById("photo-input");
+const roleSelect = document.getElementById("role-select");
 
 console.log(btnAddWorker);
 console.log(workerModal);
 
 // =========== Event Listener ===================
-btnAddWorker.addEventListener("click",(e)=>{
-    console.log("clicked");
-    workerModal.classList.remove("hidden")
-
+btnAddWorker.addEventListener("click", (e) => {
+  console.log("clicked");
+  workerModal.classList.remove("hidden");
 });
-btnCloseModal.addEventListener("click",(e)=>{
-    console.log("clicked");
-    workerModal.classList.add("hidden")
-
+btnCloseModal.addEventListener("click", (e) => {
+  console.log("clicked");
+  workerModal.classList.add("hidden");
 });
+
+btnSaveEmployee.addEventListener("click", (e) => {
+  const name = nameInput.value.trim();
+  const email = emailInput.value.trim();
+  const phone =  phoneInput.value.trim();
+  const photo =  photoInput.value.trim() || "default photo";
+  const  role = roleSelect.value;
+
+  employees.push({
+    id: Date.now(),
+    name:name,
+    phone:phone,
+    email:email,
+    role:role,
+    experiences:{}
+  });
+
+  workerModal.classList.add("hidden");
+  clearModalFields();
+  saveToLocalStorage();
+  console.log(employees);
+});
+
+// ================== Functions =============
+
+function clearModalFields(){
+    nameInput.value = "";
+    emailInput.value = "";
+    phoneInput.value = "";
+    photoInput.value = "";
+    // clear experiences
+}
+
+function saveToLocalStorage(){
+    localStorage.setItem("employees",JSON.stringify(employees));
+}
