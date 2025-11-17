017713e105395ed314541efa28b4142ca47981cf
hamzalafsioui <hamzalafsioui@gmail.com>
2025-11-17 16:28:03 +0100

    implement structure of script.js & select all zones

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,25 @@
 // Global variables
+let employees = [];
+let zones = document.querySelectorAll(".zone");
+console.log(zones);
+// ============== Local Storage ================
+
+
+// ================= Modals ===================
 const workerModal = document.getElementById("add-worker-modal");
+
+
+// ================= Buttons ===================
 const btnAddWorker = document.getElementById("open-modal-btn");
 const btnCloseModal = document.getElementById("btn-close-modal");
+
+// ================= Input =====================
+
+
 console.log(btnAddWorker);
 console.log(workerModal);
 
+// =========== Event Listener ===================
 btnAddWorker.addEventListener("click",(e)=>{
     console.log("clicked");
     workerModal.classList.remove("hidden")
